167e83c7cbeee8b4ba5230c794e09dbd2efdce88
ruuffian <ruuffian@gmail.com>
2021-12-07 19:32:30 -0500

    refactored day 7 to be DP compliant

--- app/src/main/java/Day4.java
+++ app/src/main/java/Day4.java
@@ -1,16 +1,17 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Day4 {
-    private final List<Board> boards = new ArrayList<>();
-    private final String[] order;
+    private final List<Integer[][]> boards = new ArrayList<>();
+    private final int[] order;
 
     public Day4(File inputFile) throws FileNotFoundException {
         Scanner scanner = new Scanner(inputFile);
-        order = scanner.nextLine().split(",");
+        order = Arrays.stream(scanner.nextLine().split(",")).mapToInt(Integer::parseInt).toArray();
         while (scanner.hasNext()) {
             Integer[][] board = new Integer[5][5];
             for (int i = 0; i < 5; i++) {
@@ -18,102 +19,80 @@ public class Day4 {
                     board[i][j] = scanner.nextInt();
                 }
             }
-            boards.add(new Board(board));
+            boards.add(board);
         }
     }
 
     public int partOne() {
-        for (String num : order) {
-            for (Board board : boards) {
-                int[] cord = searchBoard(board, Integer.parseInt(num));
+        for (int num : order) {
+            for (Integer[][] board : boards) {
+                int[] cord = searchBoard(board, num);
                 if (cord[0] != -1) {
-                    board.selectBoard[cord[0]][cord[1]] = true;
+                    board[cord[0]][cord[1]] = -1;
                 }
-                if (boardComplete(board.selectBoard)) {
-                    return returnUnmarked(board) * Integer.parseInt(num);
+                if (boardComplete(board)) {
+                    return returnUnmarked(board) * num;
                 }
             }
         }
         return 0;
     }
 
-    public int[] searchBoard(Board board, int target) {
-        for (int i = 0; i < board.valueBoard.length; i++) {
-            for (int j = 0; j < board.valueBoard.length; j++) {
-                if (board.valueBoard[i][j] == target)
+    public int partTwo() {
+        for (int num : order) {
+            for (int i = 0; i < boards.size(); i++) {
+                Integer[][] board = boards.get(i);
+                int[] cord = searchBoard(board, num);
+                if (cord[0] != -1) {
+                    board[cord[0]][cord[1]] = -1;
+                }
+                if (boardComplete(board)) {
+                    if (boards.size() > 1) {
+                        boards.remove(board);
+                        i--;
+                    } else
+                        return returnUnmarked(board) * num;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public int[] searchBoard(Integer[][] board, int target) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == target)
                     return new int[]{i, j};
             }
         }
         return new int[]{-1, -1};
     }
 
-    public boolean boardComplete(boolean[][] board) {
+    public boolean boardComplete(Integer[][] board) {
         for (int i = 0; i < board.length; i++) {
-            int count = 0;
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j])
-                    count++;
+            int rowCount = 0;
+            int colCount = 0;
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == -1)
+                    rowCount++;
+                if (board[j][i] == -1)
+                    colCount++;
             }
-            if (count == 5)
-                return true;
-        }
-        // PLEASE FOR THE LOVE OF GOD REWRITE THIS METHOD
-        for (int j = 0; j < board.length; j++) {
-            int count = 0;
-            for (int i = 0; i < board.length; i++) {
-                if (board[i][j])
-                    count++;
-            }
-            if (count == 5)
+            if (rowCount == 5 || colCount == 5)
                 return true;
         }
         return false;
     }
 
-    public int returnUnmarked(Board board) {
+    public int returnUnmarked(Integer[][] board) {
         int sum = 0;
-        for (int i = 0; i < board.selectBoard.length; i++) {
-            for (int j = 0; j < board.selectBoard.length; j++) {
-                if (!board.selectBoard[i][j]) {
-                    sum += board.valueBoard[i][j];
+        for (Integer[] row : board) {
+            for (Integer val : row) {
+                if (val != -1) {
+                    sum += val;
                 }
             }
         }
         return sum;
-    }
-
-    public int partTwo() {
-        for (String num : order) {
-            for (int i = 0; i < boards.size(); i++) {
-                Board board = boards.get(i);
-                int[] cord = searchBoard(board, Integer.parseInt(num));
-                if (cord[0] != -1) {
-                    board.selectBoard[cord[0]][cord[1]] = true;
-                }
-                if (boardComplete(board.selectBoard)) {
-                    if (boards.size() > 1) {
-                        boards.remove(board);
-                        i--;
-                    } else
-                        return returnUnmarked(board) * Integer.parseInt(num);
-                }
-            }
-        }
-        return 0;
-    }
-}
-
-class Board {
-    Integer[][] valueBoard;
-    boolean[][] selectBoard;
-
-    public Board(Integer[][] valueBoard) {
-        this.valueBoard = valueBoard;
-        selectBoard = new boolean[valueBoard.length][valueBoard.length];
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                selectBoard[i][j] = false;
-            }
-        }
     }
 }
