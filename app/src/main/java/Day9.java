6e0935d06bd349e63970128f41dce05d324821f8
ruuffian <ruuffian@gmail.com>
2021-12-15 02:31:18 -0500

    day9 finished, pretty gross implementation of DFS and an even worse method of finding the low points. Might try and represent the ocean as a graph once I catch up on the days I have missed

--- app/src/main/java/Day9.java
+++ app/src/main/java/Day9.java
@@ -1,6 +1,11 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+
+import static java.lang.Integer.MIN_VALUE;
 
 public class Day9 {
     private final Integer[][] map = new Integer[100][100];
@@ -27,29 +32,122 @@ public class Day9 {
                 cur = map[i][j];
                 low = true;
                 if (i > 0) {
-                    if (cur >= map[i - 1][j])
-                        low = false;
+                    if (cur >= map[i - 1][j]) low = false;
                 }
                 if (i < map.length - 1) {
-                    if (cur >= map[i + 1][j])
-                        low = false;
+                    if (cur >= map[i + 1][j]) low = false;
                 }
                 if (j > 0) {
-                    if (cur >= map[i][j - 1])
-                        low = false;
+                    if (cur >= map[i][j - 1]) low = false;
                 }
                 if (j < map[i].length - 1) {
-                    if (cur >= map[i][j + 1])
-                        low = false;
+                    if (cur >= map[i][j + 1]) low = false;
                 }
-                if (low)
-                    sum += cur + 1;
+                if (low) sum += cur + 1;
             }
         }
         return sum;
     }
 
     public int partTwo() {
-        return 0;
+        int cur;
+        List<Integer[]> lowest = new ArrayList<>();
+        for (int i = 0; i < map.length; i++) {
+            boolean low;
+            for (int j = 0; j < map[i].length; j++) {
+                cur = map[i][j];
+                low = true;
+                if (i > 0) {
+                    if (cur >= map[i - 1][j]) low = false;
+                }
+                if (i < map.length - 1) {
+                    if (cur >= map[i + 1][j]) low = false;
+                }
+                if (j > 0) {
+                    if (cur >= map[i][j - 1]) low = false;
+                }
+                if (j < map[i].length - 1) {
+                    if (cur >= map[i][j + 1]) low = false;
+                }
+                if (low) lowest.add(new Integer[]{i, j});
+            }
+        }
+        int[] maxes = new int[3];
+        maxes[0] = MIN_VALUE;
+        maxes[1] = MIN_VALUE;
+        maxes[2] = MIN_VALUE;
+        for (Integer[] cords : lowest) {
+            int sum = traversal(cords[0], cords[1]);
+            Arrays.sort(maxes);
+            int tmp;
+            if (sum > maxes[0]) {
+                if (sum > maxes[1]) {
+                    if (sum > maxes[2]) {
+                        tmp = maxes[2];
+                        maxes[2] = sum;
+                        maxes[0] = maxes[1];
+                        maxes[1] = tmp;
+                    } else {
+                        tmp = maxes[1];
+                        maxes[1] = sum;
+                        maxes[0] = tmp;
+                    }
+                } else maxes[0] = sum;
+            }
+        }
+        return maxes[0] * maxes[1] * maxes[2];
+    }
+
+    public int traversal(int i, int j) {
+        return 1 + traversal(i, j, new ArrayList<>());
+    }
+
+    private int traversal(int i, int j, List<int[]> visited) {
+        int sum = 0;
+        int[] curr = new int[]{i, j};
+        if (i > 0) {
+            if (map[i - 1][j] != 9 && !hasBeenVisited(visited, i - 1, j)) {
+                visited.add(curr);
+                sum += 1 + traversal(i - 1, j, visited);
+            }
+        }
+        if (j < map[0].length - 1) {
+            if (map[i][j + 1] != 9 && !hasBeenVisited(visited, i, j + 1)) {
+                visited.add(curr);
+                sum += 1 + traversal(i, j + 1, visited);
+            }
+        }
+        if (i < map.length - 1) {
+            if (map[i + 1][j] != 9 && !hasBeenVisited(visited, i + 1, j)) {
+                visited.add(curr);
+                sum += 1 + traversal(i + 1, j, visited);
+            }
+        }
+        if (j > 0) {
+            if (map[i][j - 1] != 9 && !hasBeenVisited(visited, i, j - 1)) {
+                visited.add(curr);
+                sum += 1 + traversal(i, j - 1, visited);
+            }
+        }
+        visited.add(curr);
+        return sum;
+    }
+
+    boolean hasBeenVisited(List<int[]> visited, int i, int j) {
+        for (int[] visit : visited) {
+            if (visit[0] == i && visit[1] == j) return true;
+        }
+        return false;
+    }
+
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (Integer[] nums : map) {
+            for (Integer num : nums) {
+                builder.append(" ").append(num).append(" ");
+            }
+            builder.append('\n');
+        }
+        return builder.toString();
     }
 }
